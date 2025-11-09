274e7d83e4a7d4e0dee9a7a8eba6663b3d90df7e
Tyler Nicholas <chairman@emiratesgrouproblox.link>
2025-11-09 12:51:45 +0400

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, textAlign: "center" }}>
+      <h1>EGR Staff Portal</h1>
+      <a href="/login">Go to Login</a>
+    </div>
+  );
+}
